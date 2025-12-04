de2f371872d20455a4b96adb26239c31eb3652b0
Kacper Madalski <kacpermadalski@gmail.com>
2025-12-04 15:28:24 +0100

    changes in file names and structure for better readability2

---
+++ Lab1/tasks_1_to_3.cs
@@ -0,0 +1,51 @@
+﻿namespace Lab1;
+
+internal static class Program
+{
+    public static void Main()
+    {
+/* (task 1)
+const string password = "admin123";
+
+Console.WriteLine("Enter password:");
+string enteredPassword = Console.ReadLine()!;
+
+if (enteredPassword != password)
+{
+while (enteredPassword != password)
+{
+Console.WriteLine("Invalid password, try again!");
+enteredPassword = Console.ReadLine()!;
+}
+}
+
+Console.WriteLine("Correct password, you're logged in!");
+*/
+
+/* (task 2)
+int number;
+
+do
+{
+Console.WriteLine("Enter number higher than 0:");
+number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+Console.Write("Invalid number! ");
+}
+}
+while (number <= 0);
+
+Console.WriteLine("Exit...");
+*/
+
+/* (task 3)
+string[] cities = {"Warsaw", "Berlin", "London", "Madrid", "Paris"};
+
+foreach (string city in cities)
+{
+Console.WriteLine(city);
+}
+*/
+    }
+}
---
+++ Lab2/task_4.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab2;
+
+internal class Person
+{
+    public string Name = "";
+    public int Age;
+    public void IntroduceYourself()
+    {
+        Console.WriteLine($"I'm {Name} and I'm {Age} years old!");
+    }
+}
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Person person1 = new Person();
+        person1.Name = "John";
+        person1.Age = 31;
+
+        Person person2 = new Person();
+        person2.Name = "Mike";
+        person2.Age = 47;
+
+        Person person3 = new Person();
+        person3.Name = "Rob";
+        person3.Age = 20;
+
+        person1.IntroduceYourself();
+        person2.IntroduceYourself();
+        person3.IntroduceYourself();
+    }
+}
